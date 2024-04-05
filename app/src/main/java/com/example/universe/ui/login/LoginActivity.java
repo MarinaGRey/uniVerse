355329ef6a6b11aafb570661225ca5e43c76da5f
MarinaGRey <gomezrey.2116048@studenti.uniroma1.it>
2024-04-05 14:44:33 +0200

    add sign up button

--- app/src/main/java/com/example/universe/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/universe/ui/login/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.universe.ui.login;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.universe.MainActivity;
@@ -18,11 +19,24 @@ public class LoginActivity extends AppCompatActivity {
             getSupportActionBar().hide();
 
         // Button Log-in -> Go to main activity
-        View button = findViewById(R.id.button);
-        button.setOnClickListener(view -> {
+        View login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(view -> {
             Intent intent= new Intent(getBaseContext(), MainActivity.class);
             startActivity(intent);
+        });
 
+        View sign_up_button = findViewById(R.id.sign_up_button);
+        EditText username_text = findViewById(R.id.username_text);
+        username_text.setVisibility(View.GONE);
+
+        sign_up_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Toggle visibility of map modal layout
+                if (username_text.getVisibility() == View.GONE) {
+                    username_text.setVisibility(View.VISIBLE);
+                }
+            }
         });
 
     }
