68dceca51145958dd172dd0c7ec9ac166d8e8121
MarinaGRey <gomezrey.2116048@studenti.uniroma1.it>
2024-04-12 13:59:15 +0200

    sig-up works

--- app/src/main/java/com/example/universe/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/universe/ui/login/LoginActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.universe.MainActivity;
 import com.example.universe.R;
-
+import com.example.universe.ui.map.MapFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
@@ -45,7 +52,31 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // Initialize Firebase Auth
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        confirm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText email = findViewById(R.id.editTextTextEmailAddress);
+                EditText password = findViewById(R.id.editTextTextPassword);
+
+                mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // User creation success
+                                    Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                                    startActivity(intent);
+                                } else {
+                                    // User creation failed
+                                    Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
-
-
 }
