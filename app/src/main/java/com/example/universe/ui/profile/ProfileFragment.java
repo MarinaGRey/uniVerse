5b69cf3681bbcb92af2793945a3ae49bda46273b
olgadelpozo <bonacheradelpozoolga@gmail.com>
2024-05-05 02:41:33 +0200

    little modification on profile

--- app/src/main/java/com/example/universe/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/example/universe/ui/profile/ProfileFragment.java
@@ -61,11 +61,6 @@ public class ProfileFragment extends Fragment {
 
 
 
-
-
-
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
